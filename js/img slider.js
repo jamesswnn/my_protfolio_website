ea2147da9c40fe9fc9bbd80029c8e650881abc5f
Jarabonn <thitisakipad@gmail.com>
2025-03-27 18:39:49 +0700

    fix final

--- js/img slider.js
+++ js/img slider.js
@@ -1,9 +1,9 @@
 /* JavaScript for Image Slider */
 const images = [
-    "/assets/project-img/ColorJourney/1.png",
-    "/assets/project-img/ColorJourney/2.png",
-    "/assets/project-img/ColorJourney/3.png",
-    "/assets/project-img/ColorJourney/4.png"
+    "assets/project-img/ColorJourney/1.png",
+    "assets/project-img/ColorJourney/2.png",
+    "assets/project-img/ColorJourney/3.png",
+    "assets/project-img/ColorJourney/4.png"
 ];
 let index = 0;
 function changeImage() {
