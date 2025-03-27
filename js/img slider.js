5063c45752b61316f674d73129f875676dc70b80
Jarabonn <thitisakipad@gmail.com>
2025-03-27 15:46:35 +0700

    project  all page
    but still not Responsive

---
+++ js/img slider.js
@@ -0,0 +1,13 @@
+/* JavaScript for Image Slider */
+const images = [
+    "/assets/project-img/ColorJourney/1.png",
+    "/assets/project-img/ColorJourney/2.png",
+    "/assets/project-img/ColorJourney/3.png",
+    "/assets/project-img/ColorJourney/4.png"
+];
+let index = 0;
+function changeImage() {
+    document.getElementById("slider").src = images[index];
+    index = (index + 1) % images.length;
+}
+setInterval(changeImage, 1000); // เปลี่ยนรูปทุก 3 วินาที
